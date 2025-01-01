ed989942d67a4f6a339c7d70e463234acea982b0
Akatsumekusa <Akatsumekusa@protonmail.com>
2025-01-01 02:56:43 +0000

    resize2: Fix chdir

--- Formula/vapoursynth-resize2.rb
+++ Formula/vapoursynth-resize2.rb
@@ -1,19 +1,16 @@
 class VapoursynthResize2 < Formula
-    homepage "https://github.com/Jaded-Encoding-Thaumaturgy/vapoursynth-resize2"
-    license "ISC"
-    head "https://github.com/Jaded-Encoding-Thaumaturgy/vapoursynth-resize2.git"
-  
-    depends_on "meson" => :build
-    depends_on "ninja" => :build
-    depends_on "pkgconf" => :build
-    depends_on "vapoursynth"
-  
-    def install
-      Dir.chdir "VapourSynth" do
-        system "meson", "setup", *std_meson_args, "build"
-        system "meson", "compile", "-C", "build"
-        system "meson", "install", "-C", "build"
-      end
-    end
+  homepage "https://github.com/Jaded-Encoding-Thaumaturgy/vapoursynth-resize2"
+  license "ISC"
+  head "https://github.com/Jaded-Encoding-Thaumaturgy/vapoursynth-resize2.git"
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => :build
+  depends_on "vapoursynth"
+
+  def install
+    system "meson", "setup", *std_meson_args, "build"
+    system "meson", "compile", "-C", "build"
+    system "meson", "install", "-C", "build"
   end
-  
+end
