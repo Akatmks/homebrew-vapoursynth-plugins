2a7438fe490121df16178eb0840bc303a5eadd1a
Akatsumekusa <Akatsumekusa@protonmail.com>
2025-03-27 11:33:29 +0000

    bestsource: Let's try one hit!

---
+++ Formula/vapoursynth-bestsource.rb
@@ -0,0 +1,18 @@
+class VapoursynthBestsource < Formula
+  homepage "https://github.com/vapoursynth/bestsource"
+  license "MIT"
+  head "https://github.com/vapoursynth/bestsource.git"
+  
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => :build
+  depends_on "vapoursynth"
+  depends_on "ffmpeg"
+  depends_on "xxhash"
+
+  def install
+    system "meson", "setup", *std_meson_args, "build"
+    system "meson", "compile", "-C", "build"
+    system "meson", "install", "-C", "build"
+  end
+end
